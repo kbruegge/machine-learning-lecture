43e7f305ef2ce833c603e88638274a0a5e6212de
Kai B <kai.bruegge@tu-dortmund.de>
2017-11-30 20:28:33 +0100

    nnn

--- ml/plots.py
+++ ml/plots.py
@@ -24,6 +24,20 @@ def set_plot_style():
     plt.rcParams['axes.spines.top'] = False
     plt.rcParams['axes.spines.right'] = False
 
+    
+
+def twospirals(n_samples, noise=.5):
+    """
+     Returns the two spirals dataset.
+    """
+    n = np.sqrt(np.random.rand(n_samples,1)) * 360 * (2*np.pi)/360
+    d1x = -np.cos(n)*n + np.random.rand(n_samples,1) * noise
+    d1y = np.sin(n)*n + np.random.rand(n_samples,1) * noise
+    return (np.vstack((np.hstack((d1x,d1y)),np.hstack((-d1x,-d1y)))), 
+            np.hstack((np.zeros(n_samples),np.ones(n_samples))))
+
+    
+
 
 def draw_linear_regression_function(reg, ax=None, **kwargs):
     if not ax:
