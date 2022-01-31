dcda8a57db373a74edbbfdc248f781249f75b0e1
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-01-31 14:36:54 +0100

    Add new function for colormap and use new random API

--- ml/plots.py
+++ ml/plots.py
@@ -9,8 +9,16 @@ import pandas as pd
 from matplotlib.colors import ListedColormap, to_hex
 
 
+rng = np.random.default_rng(0)
+
+
 colors = ['xkcd:sky', 'xkcd:grass']
 cmap = ListedColormap(colors)
+
+def create_discrete_colormap(n_classes):
+    if n_classes == 2:
+        return cmap.copy()
+    return ListedColormap([f'C{i}' for i in range(n_classes)])
 
 
 def set_plot_style():
@@ -24,13 +32,13 @@ def set_plot_style():
     plt.rcParams["axes.spines.right"] = False
 
 
-def twospirals(n_samples, noise=0.5):
+def twospirals(n_samples, noise=0.5, rng=rng):
     """
      Returns the two spirals dataset.
     """
-    n = np.sqrt(np.random.rand(n_samples, 1)) * 360 * (2 * np.pi) / 360
-    d1x = -np.cos(n) * n + np.random.rand(n_samples, 1) * noise
-    d1y = np.sin(n) * n + np.random.rand(n_samples, 1) * noise
+    n = np.sqrt(rng.uniform(size=(n_samples, 1))) * 360 * (2 * np.pi) / 360
+    d1x = -np.cos(n) * n + rng.uniform((n_samples, 1)) * noise
+    d1y = np.sin(n) * n + rng.uniform((n_samples, 1)) * noise
     return (
         np.vstack((np.hstack((d1x, d1y)), np.hstack((-d1x, -d1y)))),
         np.hstack((np.zeros(n_samples), np.ones(n_samples))),
